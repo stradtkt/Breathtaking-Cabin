73a0a8f3741f85d53897e7ca3be990b0929aac33
Kevin <stradtkt22@gmail.com>
2018-01-05 23:16:47 -0500

    Added a footer

---
+++ assets/inc/footer.php
@@ -0,0 +1,12 @@
+<section id="footer-section">
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8">
+                    <h3 class="">Breathtaking Cabin</h3>
+                    <p class="">&copy; Kevin Stradtman</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+</section>
---
+++ assets/inc/home-header.php
@@ -0,0 +1,12 @@
+<section id="home-header-section">
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1 class="text-center">Breathtaking Cabin</h1>
+                    <p class="lead text-center">Sevierville TN</p>
+                </div>
+            </div>
+        </div>
+    </header>
+</section>
