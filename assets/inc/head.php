9769d5efe2611ff9219330daad12ee30ed5e5601
Kevin <stradtkt22@gmail.com>
2018-01-05 19:45:35 -0500

    Initial Commit First Page

---
+++ assets/inc/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/sass/app.css">
+        <link rel="stylesheet" href="assets/sass/app.sass">
+    </head>
+    <body>
